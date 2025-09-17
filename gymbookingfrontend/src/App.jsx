0a62b5bde1d060d284da6dc9078a3b6892d4989a
Stephen <stephenmburumburu82@gmail.com>
2025-09-17 12:49:43 +0300

    gymbookingfrontened

---
+++ gymbookingfrontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import TrainerList from '../components/TrainerList'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <TrainerList />
+    </div>
+  )
+}
+
+export default App
